3058090d35f7e65cdef424901dd0ee2d90a8daa0
lgs47_000 <lgs47_000@jiafather>
2014-11-07 01:58:19 +0900

    empty

--- src/main/resources/db/script_add.sql
+++ src/main/resources/db/script_add.sql
@@ -2,4 +2,5 @@
 alter table mydb.member add STATE varchar(1);
  
 
-alter table mydb.member add QUIT_TIME datetime DEFAULT NULL COMMENT '탈퇴시간';
+alter 
+table mydb.member add QUIT_TIME datetime DEFAULT NULL COMMENT '탈퇴시간';
